ccc309b89ffd6fc28c4fdfb97ebf8ccc6a978692
Jonatan Ns <nsualu.jonatan@gmail.com>
2026-04-16 16:40:41 +0200

    feat(pays-front) : add list and card pays

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () =>
-      import('../feature/pays/page/pays-page/pays-page').then((m) => m.PaysPage), title: 'pays'
+  {
+    path: '',
+    loadComponent: () => import('../feature/pays/page/pays-page/pays-page').then((m) => m.PaysPage),
+    title: 'Pays',
   },
-
+  {
+    path: ':uuid',
+    loadComponent: () => import('../feature/pays/components/card-pays/card-pays').then((m) => m.default),
+    title: 'Pays detail',
+  },
 ];
